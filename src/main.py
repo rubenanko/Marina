bc1e19bb4929853838d18dfb48750ca657a294ea
rubenanko <rubenanko@gmail.com>
2025-01-23 12:53:56 +0100

    update: changement de plan

--- src/main.py
+++ src/main.py
@@ -1,38 +1,30 @@
 import sys
 import abstract
 import ui
-from pynput import keyboard
 
 def main(argv : list):
     filename = argv[1]
     size = abstract.getConsoleSize()
-    cursorX,cursorY = 0,0
-
+    # cursorX,cursorY = 0,size.lines-1
+    
     abstract.clear()
-    # ui.drawFilename(filename,size.columns)
-    # ui.drawLines(size.lines-1)
+    ui.drawFilename(filename,size.columns)
+    ui.drawLines(size.lines-2)
 
     f = open(filename,"r+")
     buffer = f.read().split("\n")
     f.close()
 
-    # ui.render(buffer,size.columns-1,size.lines-1,cursorX,cursorY)
+    ui.render(buffer,size.columns-1,size.lines-2,0,size.lines-1)
 
-    with keyboard.Events() as events:
-        for event in events:
-            if event.key == keyboard.Key.esc:
-                break
-            else:
-                print('Received event {}'.format(event))
-
-    # run = True
-    # while run:
-    #     tmp,size = size,abstract.getConsoleSize()
-    #     if tmp.lines != size.lines or tmp.columns != size.columns:
-    #         ui.drawFilename(filename,size.columns)
-    #         ui.drawLines(size.lines-1)            
-    #         ui.render(buffer,size.columns,size.lines-1,cursorX,cursorY)
-
+    run = True
+    while run:
+        tmp,size = size,abstract.getConsoleSize()
+        if tmp.lines != size.lines or tmp.columns != size.columns:
+            ui.drawFilename(filename,size.columns)
+            ui.drawLines(size.lines-2)            
+            ui.render(buffer,size.columns,size.lines-2,0,size.lines-1)
+        input("\033[1;36mMarina : \033[0;0m")
 
 if __name__ == "__main__":
     main(sys.argv)
